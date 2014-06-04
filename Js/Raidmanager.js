fd538166952936cd7a99e58fe6d27299e8de268b
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-06-04 17:08:42 +0200

    rename app js file to casesensitive app name change app js events to be global

---
+++ Js/Raidmanager.js
@@ -0,0 +1,11 @@
+// Prevents submit by pressing enter
+$(document).on('keypress', '.raidmanager_form input,select', function(e){
+    if (e.keyCode == 13) {
+        return false;
+    }
+});
+
+// Copies selected user name on playercreation into field fopr the first char  
+$(document).on('change', '#raidmanager_player_smfuser', function(){
+    $('#raidmanager_char_name').val($('#raidmanager_player_smfuser option:selected').text());
+});
